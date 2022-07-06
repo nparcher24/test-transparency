1d00fbe62aef91e41b82a557f77dec0fc65d38b5
HIC P <nicholas.parrish@mac.com>
2022-07-06 08:36:04 -0400

    Initial Commit

---
+++ test-transparency/test_transparencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_transparencyApp.swift
+//  test-transparency
+//
+//  Created by Nicholas Parrish on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct test_transparencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
