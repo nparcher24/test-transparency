263c862ad09d0879bde3e87c37bcd49b172a6007
HIC P <nicholas.parrish@mac.com>
2022-07-06 20:49:39 -0400

    still troubleshooting

--- test-transparency/ContentView.swift
+++ test-transparency/ContentView.swift
@@ -6,11 +6,36 @@
 //
 
 import SwiftUI
+import AVKit
+import SpriteKit
 
 struct ContentView: View {
+    @State var mscene = SKScene()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        HStack {
+            Spacer()
+            VStack {
+                Spacer()
+                SpriteView(scene: mscene)
+                Spacer()
+            }.onAppear {
+                let scene = SKScene(size: .init(width: 1000, height: 600))
+                scene.size = CGSize(width: 1000, height: 600)
+                scene.scaleMode = .aspectFit
+                scene.backgroundColor = .clear
+                scene.view?.allowsTransparency = true
+                let player = AVPlayer(url:  Bundle.main.url(forResource: "output", withExtension: "mov")!)
+                let video = SKVideoNode(avPlayer: player)
+                video.size = CGSize(width: scene.size.width, height: scene.size.height)
+                video.anchorPoint = .init(x: 0, y: 0)
+                player.play()
+                scene.addChild(video)
+                mscene = scene
+            }
+            Spacer()
+        }.background(Color.red)
     }
 }
 
